8aeaf0e96fd852a9cebf5e30ae3e0653438e27d7
Jenbati <jen.bati16@gmail.com>
2024-11-05 13:26:54 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# git configuration
+git config --global --list
+git config --global user.name "username"
+git config --global user.email "useremail"
+
+# git initilization
+git init
+git add .
+git commit -m "initial commit"
+
+# git branch
+git branch
+
